795325d1858b9d06d272a666991d9bbe1cca48de
sanket-gandhi <sanket.cics@gmail.com>
2023-12-12 20:28:44 +0530

    macros

--- demo_dbt/models/example/my_first_dbt_model.sql
+++ demo_dbt/models/example/my_first_dbt_model.sql
@@ -21,9 +21,9 @@ with source_data as (
 
 select *
 from source_data
-
+where id is not null
 /*
     Uncomment the line below to remove records with null `id` values
 */
 
--- where id is not null
+
