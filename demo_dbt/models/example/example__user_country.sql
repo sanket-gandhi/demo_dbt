9d04fdceea38ff592714938afa8a34442cdfaf67
sanket-gandhi <sanket.cics@gmail.com>
2023-12-12 22:03:42 +0530

    query tag, pkg addition

---
+++ demo_dbt/models/example/example__user_country.sql
@@ -0,0 +1,28 @@
+
+{{ config(
+    materialized = 'table',
+    query_tag = 'dbt_demo'
+    ) }}
+
+with user_country as (
+    SELECT 123 as USER_ID, 'INDIA' AS COUNTRY
+    UNION
+    SELECT 245 as USER_ID, 'SRI-LANKA' AS COUNTRY
+    UNION
+    SELECT 890 as USER_ID, 'UK' AS COUNTRY
+    UNION
+    SELECT 765 as USER_ID, 'US' AS COUNTRY
+    UNION
+    SELECT 345 as USER_ID, 'AFRICA' AS COUNTRY
+),
+user_join as (
+    SELECT t1.COUNTRY, t2.* 
+    FROM user_country as t1 
+    inner join {{ ref('example__user_csv') }} as t2 
+    on t1.user_id = t2.user_id
+),
+final as (
+    select COUNTRY = '{{ var('country_champion') }}' as  champion, *
+    from user_join
+)
+select * from final
--- demo_dbt/models/example/example__user_csv.sql
+++ demo_dbt/models/example/example__user_csv.sql
@@ -2,6 +2,6 @@ with main_user as (
     select * from {{ref('user')}}
 ),
 final as (
-    select * from main_user where user_id > 500
+    select * from main_user
 )
 select * from final
